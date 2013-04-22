065046abdb24224fd49141bd6d5d67b5488045cf
Ryan Rauh <rauh.ryan@gmail.com>
2013-04-22 11:23:28 -0500

    quick hack to get the gem working

--- Rakefile
+++ Rakefile
@@ -1,1 +1,10 @@
 require "bundler/gem_tasks"
+
+$LOAD_PATH.unshift File.expand_path("../lib", __FILE__)
+
+require "fubu/ripple"
+
+desc "Update cli"
+task :update do 
+  Ripple.new.update
+end
---
+++ lib/fubu/ripple.rb
@@ -0,0 +1,24 @@
+require "thor"
+
+class Ripple < Thor
+  include Thor::Actions
+
+  def self.source_root
+    File.expand_path "../../..", __FILE__
+  end
+
+  desc "update the nugets", "runs ripple update"
+  def update
+    `#{Ripple.source_root}/buildsupport/ripple.exe update`
+    move_files
+  end
+
+  def move_files
+     Dir.glob("*/packages/**tools").each do |tool|
+       puts /packages\/(\w+)\/tools/.match(tool)[0]
+
+     end
+  end
+
+end
+
